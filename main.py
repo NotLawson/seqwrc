53128fb2b82d0596d2244384d53a50ddb05a4b58
Lawson C <lawson.conallin@gmail.com>
2025-03-10 14:02:50 +1000

    favicon, build static pages, build users

--- main.py
+++ main.py
@@ -1,73 +1,86 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, redirect
 import psycopg2
+import sys, os
+
+## GET FLAGS ##
+args = sys.argv[1:]
+if 'local' in args:
+    LOCAL = True
+else:
+    LOCAL = False
+if 'debug' in args:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ## DATABASE SETUP ##
-conn = psycopg2.connect(
-    host="db",
-    database="seqwrc",
-    user="postgres",
-    password="postgres"
-)
-cursor = conn.cursor()
-cursor.auto_commit = True
+if LOCAL == False:
+    conn = psycopg2.connect(
+        host="db",
+        database="seqwrc",
+        user="postgres",
+        password="postgres"
+    )
+    cursor = conn.cursor()
+    cursor.auto_commit = True
 
-# create tables
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    name VARCHAR(50) NOT NULL,
-    bio TEXT,
-    followers TEXT[],
-    following TEXT[],
-    posts TEXT[],
-    events TEXT[],
-    gear TEXT[],
-''')
+    # create tables
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL UNIQUE,
+        name VARCHAR(50) NOT NULL,
+        bio TEXT,
+        followers TEXT[],
+        following TEXT[],
+        posts TEXT[],
+        events TEXT[],
+        gear TEXT[],
+    ''')
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS posts (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    date DATE NOT NULL,
-    content TEXT NOT NULL,
-    likes TEXT[],
-    comments TEXT[],
-    type VARCHAR(10) NOT NULL
-''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS posts (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER REFERENCES users(id),
+        date DATE NOT NULL,
+        content TEXT NOT NULL,
+        likes TEXT[],
+        comments TEXT[],
+        type VARCHAR(10) NOT NULL
+    ''')
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS shoes (
-    id SERIAL PRIMARY KEY,
-    brand VARCHAR(50) NOT NULL,
-    model VARCHAR(50) NOT NULL,
-    review TEXT NOT NULL,
-    date DATE NOT NULL,
-    tags TEXT[]
-''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS shoes (
+        id SERIAL PRIMARY KEY,
+        brand VARCHAR(50) NOT NULL,
+        model VARCHAR(50) NOT NULL,
+        review TEXT NOT NULL,
+        date DATE NOT NULL,
+        tags TEXT[]
+    ''')
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS blog (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    date DATE NOT NULL,
-    content TEXT NOT NULL,
-    comments TEXT[],
-    tags TEXT[]
-''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS blog (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(50) NOT NULL,
+        date DATE NOT NULL,
+        content TEXT NOT NULL,
+        comments TEXT[],
+        tags TEXT[]
+    ''')
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS contact (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    message TEXT NOT NULL
-''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS contact (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        message TEXT NOT NULL
+    ''')
 
-# create admin user
-cursor.execute('''INSERT INTO users (username, password, email, name, bio, followers, following, posts, events, gear) VALUES ('admin', 'admin', 'lawson.conallin@gmail.com', 'Admin', 'Admin user. \nUsually Run by NotLawson (lawson.conallin@gmail.com).', '{}', '{}', '{}', '{}', '{}')''')
+    # create admin user
+    cursor.execute('''INSERT INTO users (username, password, email, name, bio, followers, following, posts, events, gear) VALUES ('admin', 'admin', 'lawson.conallin@gmail.com', 'Admin', 'Admin user. \nUsually Run by NotLawson (lawson.conallin@gmail.com).', '{}', '{}', '{}', '{}', '{}')''')
 
 # db functions
 def get_user(username):
@@ -76,6 +89,10 @@ def get_user(username):
 def get_user_id(user_id):
     cursor.execute(f'SELECT * FROM users WHERE id = {user_id}')
     return cursor.fetchone()
+def create_user(username, password, email, name):
+    cursor.execute(f"INSERT INTO users (username, password, email, name, bio, followers, following, posts, events, gear) VALUES ('%s', '%s', '%s', '%s', "", '{}', '{}', '{}', '{}', '{}')", (username, password, email, name))
+def delete_user(id):
+    cursor.execute(f'DELETE FROM users WHERE id = {id}')
 
 def get_post(post_id):
     cursor.execute(f'SELECT * FROM posts WHERE id = {post_id}')
@@ -109,9 +126,24 @@ def get_following_posts(username):
     ''', (following,))
     return cursor.fetchall()
 
+
+## APP SETUP ##
 app = Flask(__name__)
+TOKENS = {}
+
+def auth(request):
+    token = request.cookies.get('token')
+    if token is None:
+        return False
+    try: id = TOKENS[token]
+    except KeyError: return False
+    return id
+
 
 ## MAIN/MISC ##
+def main_not_built():
+    return render_template('not_built.html')
+
 @app.route('/')
 def main_index():
     '''
@@ -132,6 +164,12 @@ def main_contact():
     Contact page for SEQWRC
     Contains a form that logs a message to the database, which can be accessed by the admin at admin_contact (/admin/contact)
     '''
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        cursor.execute(f"INSERT INTO contact (name, email, message) VALUES ('{name}', '{email}', '{message}')")
+        return render_template('contact.html', message_good=message)
     return render_template('contact.html')
 
 @app.route('/search')
@@ -140,14 +178,14 @@ def main_search():
     Searches SEQWRC
     Mainly searches posts and users, but will also return things from Joe's Shoes
     '''
-    pass
+    return main_not_built()
 
 @app.route('/search/<query>')
 def main_search_query(query):
     '''
     See main_search
     '''
-    pass
+    return main_not_built()
 
 ## ACCOUNTS ##
 @app.route('/login', methods=['GET', 'POST'])
@@ -157,21 +195,52 @@ def accounts_login():
     An account is only needed to post, follow, and comment on things on the website. 
     Other things don't require an account.
     '''
-    pass
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = get_user(username) # get user object from the database
+        if user == None: # username not in database
+            return render_template('login.html', message_bad="User not found")
+        if user[2] == password:
+            # logged in
+            token = os.urandom(16).hex()  # gen user id
+            TOKENS[token] = user[0] # add to token store along with userId
+
+            url = request.args.get('next', '/') # get the redirect url
+            resp = make_response(redirect(url))
+            resp.set_cookie('token', token) # set cookie
+            return resp
+        return render_template('login.html', message_bad="Incorrect password") # incorrect password
+    return render_template('login.html') # login page
+
 @app.route('/signup', methods=['GET', 'POST'])
 def accounts_signup():
     '''
     Signup page for SEQWRC
     See accounts_login for more info on accounts
     '''
-    pass
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        name = request.form['name']
+        try: 
+            create_user(username, password, email, name)
+            return redirect('/login')
+        except:
+            return render_template('signup.html', message_bad="User already exists")
+    return render_template('signup.html')
+
 @app.route('/logout')
 def accounts_logout():
     '''
     Signout endpoint for SEQWRC
     Redirects to main_index (/)
     '''
-    pass
+    resp = make_response(redirect('/'))
+    resp.set_cookie('token', '', expires=0)
+    return resp
+
 @app.route('/account', methods=['GET', 'POST', 'DELETE'])
 def accounts_account():
     '''
@@ -179,7 +248,25 @@ def accounts_account():
     Allows users to edit account details and delete their account
     See admin_login for more info on accounts
     '''
-    pass
+    id = auth(request)
+    user = get_user_id(id)
+    if user == None:
+        return redirect('/login?next=/account')
+    
+    if request.method == "DELETE":
+        # (endpoint)
+        delete_user(id)
+        return "done"
+    if request.method == "POST":
+        # edit account
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        name = request.form['name']
+
+        cursor.execute(f"UPDATE users SET username = '{username}', password = '{password}', email = '{email}', name = '{name}' WHERE id = {id}")
+        return render_template('account.html', user=user, message_good="Account updated")
+    return render_template('account.html', user=user)
 
 ## SOCIAL ##
 @app.route('/feed')
@@ -201,7 +288,7 @@ def social_feed():
     
     This page will display posts from users that the current user follows, aswell as all posts in a seperate tab.
     '''
-    pass
+    return main_not_built()
 
 @app.route('/feed/new', methods=['GET', 'POST'])
 def social_new_post():
@@ -209,7 +296,7 @@ def social_new_post():
     Creates a new post
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/feed/<post_id>', methods=['GET', 'POST'])
 def social_post(post_id):
@@ -217,7 +304,7 @@ def social_post(post_id):
     Displays a specific post
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/feed/<post_id>/edit', methods=['GET', 'POST', 'DELETE'])
 def social_edit_post(post_id):
@@ -226,7 +313,7 @@ def social_edit_post(post_id):
     Also deletes a post
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/feed/<post_id>/like', methods=['GET', 'DELETE'])
 def social_like_post(post_id):
@@ -235,7 +322,7 @@ def social_like_post(post_id):
     Also remove like from a post
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/feed/<post_id>/comment', methods=['POST', 'DELETE'])
 def social_comment_post(post_id):
@@ -244,7 +331,7 @@ def social_comment_post(post_id):
     Also deletes a comment
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/profile', methods=['GET', 'POST'])
 def social_all_profiles():
@@ -269,7 +356,7 @@ def social_all_profiles():
 
     This page will display diplay a search bar, current followers/following, recomended users
     '''
-    pass
+    return main_not_built()
 
 @app.route('/profile/<username>', methods=['GET', 'POST'])
 def social_profile(username):
@@ -277,7 +364,7 @@ def social_profile(username):
     Displays a specific profile
     See social_all_profiles for more info on profiles
     '''
-    pass
+    return main_not_built()
 
 @app.route('/profile/<username>/follow', methods=['GET', 'DELETE'])
 def social_follow(username):
@@ -286,7 +373,7 @@ def social_follow(username):
     Also unfollows a user
     See social_all_profiles for more info on profiles
     '''
-    pass
+    return main_not_built()
 
 @app.route('/profile/me/edit', methods=['GET', 'POST'])
 def social_edit_profile():
@@ -294,7 +381,7 @@ def social_edit_profile():
     Edits your profile
     See social_all_profiles for more info on profiles
     '''
-    pass
+    return main_not_built()
 
 ## JOE'S SHOES ##
 @app.route('/joe')
@@ -309,7 +396,7 @@ def joe_index():
     This page will display the latest blog post and the latest shoe review, aswell as links to each subpage
     See joe_shoes and joe_blog for more info on each part
     '''
-    pass
+    return main_not_built()
 @app.route('/joe/shoes')
 def joe_shoes():
     '''
@@ -324,7 +411,7 @@ def joe_shoes():
 
     This page will display all shoes in the database, as well as a search and filtering feature
     '''
-    pass
+    return main_not_built()
 
 @app.route('/joe/shoes/<shoe_id>')
 def joe_shoe(shoe_id):
@@ -332,7 +419,7 @@ def joe_shoe(shoe_id):
     Displays a specific shoe
     See joe_shoes for more info on shoes
     '''
-    pass
+    return main_not_built()
 
 @app.route('/joe/blog')
 def joe_blog():
@@ -348,7 +435,7 @@ def joe_blog():
 
     This page will display all blog posts in the database, as well as a search and filtering feature
     '''
-    pass
+    return main_not_built()
 
 @app.route('/joe/blog/<post_id>')
 def joe_post(post_id):
@@ -356,7 +443,7 @@ def joe_post(post_id):
     Displays a specific post
     See joe_blog for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/joe/blog/<post_id>/comment', methods=['POST', 'DELETE'])
 def joe_comment_post(post_id):
@@ -366,7 +453,7 @@ def joe_comment_post(post_id):
     Also deletes a comment
     See joe_blog for more info on posts
     '''
-    pass
+    return main_not_built()
 
 ## ADMIN ##
 @app.route('/admin', methods=['GET', 'POST'])
@@ -375,7 +462,7 @@ def admin_admin():
     Admin page for SEQWRC
     This section allows for admins to view and control users accounts, posts, the contact form and Joe's Shoes
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/users', methods=['GET', 'POST'])
 def admin_users():
@@ -383,7 +470,7 @@ def admin_users():
     Displays all users
     See main_login for more info on accounts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/users/<username>', methods=['GET', 'POST'])
 def admin_user(username):
@@ -391,7 +478,7 @@ def admin_user(username):
     Displays a specific user
     See main_login for more info on accounts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/users/<username>/edit', methods=['GET', 'POST', 'DELETE'])
 def admin_edit_user(username):
@@ -400,7 +487,7 @@ def admin_edit_user(username):
     Also deletes a user
     See main_login for more info on accounts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/posts', methods=['GET', 'POST'])
 def admin_posts():
@@ -408,7 +495,7 @@ def admin_posts():
     Displays all posts
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/posts/<post_id>', methods=['GET', 'POST', 'DELETE'])
 def admin_post(post_id):
@@ -416,7 +503,7 @@ def admin_post(post_id):
     Displays a specific post
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/posts/<post_id>/edit', methods=['GET', 'POST', 'DELETE'])
 def admin_edit_post(post_id):
@@ -425,7 +512,7 @@ def admin_edit_post(post_id):
     Also deletes a post
     See social_feed for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe')
 def admin_joe():
@@ -434,7 +521,7 @@ def admin_joe():
     Link to each subpage, and show engagement stats
     See joe_shoes and joe_blog for more info on each part
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe/shoes')
 def admin_joe_shoes():
@@ -442,7 +529,7 @@ def admin_joe_shoes():
     Shows all shoes in the database
     See joe_shoes for more info on shoes
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe/shoes/new', methods=['GET', 'POST'])
 def admin_joe_new_shoe():
@@ -450,7 +537,7 @@ def admin_joe_new_shoe():
     Creates a new shoe
     See joe_shoes for more info on shoes
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe/shoes/<shoe_id>', methods=['GET', 'POST', 'DELETE'])
 def admin_joe_shoe(shoe_id):
@@ -460,7 +547,7 @@ def admin_joe_shoe(shoe_id):
     Also edits a shoe
     Also deletes a shoe
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe/blog')
 def admin_joe_blog():
@@ -468,7 +555,7 @@ def admin_joe_blog():
     Shows all blog posts in the database
     See joe_blog for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe/blog/new', methods=['GET', 'POST'])
 def admin_joe_new_post():
@@ -476,7 +563,7 @@ def admin_joe_new_post():
     Creates a new post
     See joe_blog for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe/blog/<post_id>', methods=['GET', 'POST', 'DELETE'])
 def admin_joe_post(post_id):
@@ -484,7 +571,7 @@ def admin_joe_post(post_id):
     Displays a specific post'
     See joe_blog for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/joe/blog/<post_id>/edit', methods=['GET', 'POST', 'DELETE'])
 def admin_joe_edit_post(post_id):
@@ -493,7 +580,7 @@ def admin_joe_edit_post(post_id):
     Also deletes a post
     See joe_blog for more info on posts
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/contact')
 def admin_contact():
@@ -501,7 +588,7 @@ def admin_contact():
     Shows all contact tickets from the contact form
     See main_contact for more info on the contact form
     '''
-    pass
+    return main_not_built()
 
 @app.route('/admin/contact/<message_id>', methods=['GET', 'DELETE'])
 def admin_contact_message(message_id):
@@ -510,6 +597,6 @@ def admin_contact_message(message_id):
     Also deletes a message
     See main_contact for more info on the contact form
     '''
-    pass
+    return main_not_built()
 
-app.run(debug=True)
+app.run("0.0.0.0", 5000, debug=True)
--- static/index.js
+++ static/index.js
@@ -1,56 +1,100 @@
+async function write(string) {
+    for (let i = 0; i < string.length; i++) {
+        title.innerHTML = string.slice(0, i + 1);
+        await new Promise(r => setTimeout(r, 100));
+    }
+    await new Promise(r => setTimeout(r, 5000));
+    for (let i = title.innerHTML.length; i >= 0; i--) {
+        title.innerHTML = title.innerHTML.slice(0, -1);
+        await new Promise(r => setTimeout(r, 50));
+    }
+}
+
 async function title() {
     const title = document.getElementById('name');
 
-    const strings = ["Your local running club", "The best", "The worst running club in South East Queenland", "South East Queenslands Worst Running Club"]
-
+    const strings = ["South East Queensland Worst Running Club", "Your local running club", "Your gateway to start your running journey", "A vibrant community", "The worst running club in South East Queenland", "South East Queenslands Worst Running Club"]
     while (true) {
-        await new Promise(r => setTimeout(r, 1000));
-        // backspacing
-        for (let i = title.innerHTML.length; i >= 0; i--) {
-            title.innerHTML = title.innerHTML.slice(0, -1);
-            await new Promise(r => setTimeout(r, 50));
-        }
-        // typing
-        // Your local running club
-        await new Promise(r => setTimeout(r, 500));
-        for (let i = 0; i < strings[0].length; i++) {
-            title.innerHTML = strings[0].slice(0, i + 1);
-            await new Promise(r => setTimeout(r, 100));
-        }
-        await new Promise(r => setTimeout(r, 5000));
-        // backspacing
-        for (let i = title.innerHTML.length; i >= 0; i--) {
-            title.innerHTML = title.innerHTML.slice(0, -1);
-            await new Promise(r => setTimeout(r, 50));
-        }
 
-        // typing
-        // The best
-        await new Promise(r => setTimeout(r, 500));
-        for (let i = 0; i < strings[1].length; i++) {
-            title.innerHTML = strings[1].slice(0, i + 1);
+        // 1st: South East Queensland Worst Running Club
+        var string = strings[0];
+        for (let i = 0; i < string.length; i++) {
+            title.innerHTML = string.slice(0, i + 1);
             await new Promise(r => setTimeout(r, 100));
         }
-        // backspacing
-        for (let i = 4; i >= 0; i--) {
+        await new Promise(r => setTimeout(r, 3000));
+        for (let i = title.innerHTML.length; i >= 0; i--) {
             title.innerHTML = title.innerHTML.slice(0, -1);
             await new Promise(r => setTimeout(r, 50));
         }
         await new Promise(r => setTimeout(r, 1000));
 
-        // typing
-        // the worst running club in South East Queenland
-        await new Promise(r => setTimeout(r, 500));
-        for (let i = 0; i < strings[2].length; i++) {
-            title.innerHTML = strings[2].slice(0, i + 1);
+        // 2nd: Your local running club
+        var string = strings[1];
+        for (let i = 0; i < string.length; i++) {
+            title.innerHTML = string.slice(0, i + 1);
             await new Promise(r => setTimeout(r, 100));
         }
-        // backspacing
+        await new Promise(r => setTimeout(r, 3000));
         for (let i = title.innerHTML.length; i >= 0; i--) {
             title.innerHTML = title.innerHTML.slice(0, -1);
             await new Promise(r => setTimeout(r, 50));
         }
+        await new Promise(r => setTimeout(r, 1000));
+
+        // 3rd: Your gateway to start your running journey
+        var string = strings[2];
+        for (let i = 0; i < string.length; i++) {
+            title.innerHTML = string.slice(0, i + 1);
+            await new Promise(r => setTimeout(r, 100));
+        }
+        await new Promise(r => setTimeout(r, 3000));
+        for (let i = title.innerHTML.length; i >= 0; i--) {
+            title.innerHTML = title.innerHTML.slice(0, -1);
+            await new Promise(r => setTimeout(r, 50));
+        }
+        await new Promise(r => setTimeout(r, 1000));
+        
+        // 4th: a Vibrant community
+        var string = strings[3];
+        for (let i = 0; i < string.length; i++) {
+            title.innerHTML = string.slice(0, i + 1);
+            await new Promise(r => setTimeout(r, 100));
+        }
+        await new Promise(r => setTimeout(r, 3000));
+        for (let i = title.innerHTML.length; i >= 0; i--) {
+            title.innerHTML = title.innerHTML.slice(0, -1);
+            await new Promise(r => setTimeout(r, 50));
+        }
+        await new Promise(r => setTimeout(r, 1000));
+
+        // 5th: The worst running club in South East Queenland
+        var string = strings[4];
+        for (let i = 0; i < string.length; i++) {
+            title.innerHTML = string.slice(0, i + 1);
+            await new Promise(r => setTimeout(r, 100));
+        }
+        await new Promise(r => setTimeout(r, 3000));
+        for (let i = title.innerHTML.length; i >= 0; i--) {
+            title.innerHTML = title.innerHTML.slice(0, -1);
+            await new Promise(r => setTimeout(r, 50));
+        }
+        await new Promise(r => setTimeout(r, 1000));
+
+        // 6th: South East Queenslands Worst Running Club
+        var string = strings[5];
+        for (let i = 0; i < string.length; i++) {
+            title.innerHTML = string.slice(0, i + 1);
+            await new Promise(r => setTimeout(r, 100));
+        }
+        await new Promise(r => setTimeout(r, 3000));
+        for (let i = title.innerHTML.length; i >= 0; i--) {
+            title.innerHTML = title.innerHTML.slice(0, -1);
+            await new Promise(r => setTimeout(r, 50));
+        }
+        await new Promise(r => setTimeout(r, 1000));
     }
 }
+
 
 title();
