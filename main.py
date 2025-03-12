341f2a47686981438c89b1c826014b1d8bf7028f
Lawson C <lawson.conallin@gmail.com>
2025-03-12 17:45:30 +1000

    debug: get the contents of the get_all_posts() function

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ def social_feed():
         posts = []
     all_posts = get_all_posts()
     if all_posts==None:
-        all_posts = []
+        all_posts = ()
     return render_template('feed.html', user=user, posts=posts, all_posts=all_posts, get_user_id=get_user_id, json=json)
 
 @app.route('/feed/new', methods=['GET', 'POST'])
